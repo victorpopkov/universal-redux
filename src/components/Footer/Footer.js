788edc8ae65bfeb9b9195a7ff3ac4ef3a65f5aff
Victor Popkov <victor@popkov.me>
2018-05-10 04:29:48 +0300

    Improve some responsiveness on small screens

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,22 +7,24 @@ const Footer = () => (
       <ul>
         <li><a href="https://github.com/victorpopkov/isomorphic-redux-reactstrap">View on GitHub</a></li>
       </ul>
-      <iframe
-        frameBorder="0"
-        height="20px"
-        scrolling="0"
-        src="https://ghbtns.com/github-btn.html?user=victorpopkov&repo=isomorphic-redux-reactstrap&type=star&count=true"
-        title="GitHub Stars"
-        width="80px"
-      />
-      <iframe
-        frameBorder="0"
-        height="20px"
-        scrolling="0"
-        src="https://ghbtns.com/github-btn.html?user=victorpopkov&repo=isomorphic-redux-reactstrap&type=watch&count=true&v=2"
-        title="GitHub Watchers"
-        width="90px"
-      />
+      <div styleName="buttons">
+        <iframe
+          frameBorder="0"
+          height="20px"
+          scrolling="0"
+          src="https://ghbtns.com/github-btn.html?user=victorpopkov&repo=isomorphic-redux-reactstrap&type=star&count=true"
+          title="GitHub Stars"
+          width="80px"
+        />
+        <iframe
+          frameBorder="0"
+          height="20px"
+          scrolling="0"
+          src="https://ghbtns.com/github-btn.html?user=victorpopkov&repo=isomorphic-redux-reactstrap&type=watch&count=true&v=2"
+          title="GitHub Watchers"
+          width="90px"
+        />
+      </div>
     </div>
   </footer>
 );

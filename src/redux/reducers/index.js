5f9bedcbf58b9333cc4709362a044881e04c97a1
Victor Popkov <victor@popkov.me>
2018-05-13 21:02:07 +0300

    Use default routerReducer in Redux reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,30 +3,17 @@ import {
   setToImmutableStateFunc,
   setToMutableStateFunc,
 } from 'redux-connect';
-import { LOCATION_CHANGE } from 'react-router-redux';
 import { combineReducers } from 'redux-immutablejs';
 import { fromJS } from 'immutable';
+import { routerReducer } from 'react-router-redux';
 import eventsReducer from './events'; // eslint-disable-line sort-imports
 import markdownReducer from './markdown';
-
-const initialState = fromJS({
-  locationBeforeTransitions: null,
-});
 
 // Set the mutability/immutability functions
 setToImmutableStateFunc(mutableState => fromJS(mutableState));
 setToMutableStateFunc(immutableState => immutableState.toJS());
 
-const routerReducer = (state = initialState, action) => {
-  if (action.type === LOCATION_CHANGE) {
-    return state.set('locationBeforeTransitions', action.payload);
-  }
-
-  return state;
-};
-
-const entities = (state = fromJS({
-}), action) => {
+const entities = (state = fromJS({}), action) => {
   if (action.result && action.result.entities) {
     return state.mergeDeep(action.result.entities);
   }
@@ -35,9 +22,9 @@ const entities = (state = fromJS({
 };
 
 export default combineReducers({
-  routing: routerReducer,
-  reduxAsyncConnect,
-  entities,
   events: eventsReducer,
   markdown: markdownReducer,
+  router: routerReducer,
+  reduxAsyncConnect,
+  entities,
 });

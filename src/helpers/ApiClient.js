11e41ff2d05ccff3e1641c80f71a4896f5785b9c
Victor Popkov <victor@popkov.me>
2018-09-17 10:11:48 +0300

    Update ApiClient helper

--- src/helpers/ApiClient.js
+++ src/helpers/ApiClient.js
@@ -1,55 +1,89 @@
-// import Cookies from 'universal-cookie';
+import Cookies from 'universal-cookie';
 import axios from 'axios';
 import { camelizeKeys } from 'humps';
 import { normalize } from 'normalizr';
 import config from '@Config'; // eslint-disable-line sort-imports
 
-const methods = ['get', 'post', 'put', 'patch', 'del'];
-
-function formatUrl(path) {
-  const adjustedPath = path[0] !== '/' ? `/${path}` : path;
-
-  if (__SERVER__) {
-    // Prepend host and port of the API server to the path.
-    return `${config.apiSchema}://${config.apiHost}:${config.apiPort + config.apiPrefix + adjustedPath}`;
-  }
-
-  // Prepend `/api` to relative URL, to proxy to API server.
-  return `/api${adjustedPath}`;
-}
-
 export default class ApiClient {
-  // eslint-disable-next-line no-unused-vars
-  constructor(req) {
-    // const cookies = (req && req.universalCookies) || new Cookies();
+  static methods = ['get', 'post', 'put', 'patch', 'del'];
 
-    methods.forEach(method => ( // eslint-disable-line no-return-assign
+  formatUrl = (path) => {
+    const adjustedPath = path[0] !== '/' ? `/${path}` : path;
+    return __SERVER__ ? `${config.proxyApiTarget + adjustedPath}` : `/api${adjustedPath}`;
+  };
+
+  constructor(req) {
+    const cookies = (req && req.universalCookies) || new Cookies();
+
+    ApiClient.methods.forEach(method => ( // eslint-disable-line no-return-assign
       this[method] = (path, {
         params,
         data,
-        // files,
+        files,
         schema,
         cancelToken,
       } = {}) => new Promise((resolve, reject) => {
-        const request = {
+        let request = {
           method,
-          url: formatUrl(path),
+          url: this.formatUrl(path),
           params,
           data,
           cancelToken,
         };
 
-        // TODO: Implement files upload using axios
-        // if (files && files.files && files.files.length > 0) {
-        //   files.files.forEach((f) => {
-        //     request.attach(files.name || f.name, f);
-        //   });
-        // }
+        const token = cookies.get('token');
 
-        axios.request(request)
+        if (token) {
+          request = {
+            ...request,
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          };
+        }
+
+        if (files && files.files && files.files.length > 0) {
+          const formData = new FormData();
+          files.files.forEach((f) => {
+            formData.append(files.name || f.name, f);
+          });
+
+          axios
+            .post(request.url, formData, {
+              headers: {
+                ...request.headers,
+                'Content-Type': 'multipart/form-data',
+              },
+            })
+            .then((response) => {
+              if (token && response.status === 401) {
+                cookies.remove('token');
+              }
+
+              const camelized = camelizeKeys(response.data);
+              resolve((schema) ? normalize(camelized, schema) : camelized);
+            })
+            .catch((error) => {
+              if (!axios.isCancel(error)) {
+                if (error.response) {
+                  reject(camelizeKeys(error.response.data) || error.response);
+                } else {
+                  reject(error);
+                }
+              }
+            });
+
+          return;
+        }
+
+        axios
+          .request(request)
           .then((response) => {
-            const camelized = camelizeKeys(response.data);
+            if (token && response.status === 401) {
+              cookies.remove('token');
+            }
 
+            const camelized = camelizeKeys(response.data);
             resolve((schema) ? normalize(camelized, schema) : camelized);
           })
           .catch((error) => {

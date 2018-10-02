3a0e618fa90d34a6686dc3c8646cb850a0731789
Victor Popkov <victor@popkov.me>
2018-10-02 05:52:26 +0300

    Add "Access-Control-Allow-Origin" with "*" in api

--- api/api.js
+++ api/api.js
@@ -8,34 +8,37 @@ import mapUrl from './utils/url';
 const pretty = new PrettyError();
 const app = new Express();
 
-app.use(bodyParser.json());
-app.use((req, res) => {
-  const splittedUrlPath = req.url.split('?')[0].split('/').slice(1);
-  const { action, params } = mapUrl(actions, splittedUrlPath);
+app
+  .use(bodyParser.json())
+  .use((req, res) => {
+    const splittedUrlPath = req.url.split('?')[0].split('/').slice(1);
+    const { action, params } = mapUrl(actions, splittedUrlPath);
 
-  if (action) {
-    action(req, params)
-      .then((result) => {
-        if (result instanceof Function) {
-          result(res);
-        } else {
-          res.json(result);
-        }
-      }, (reason) => {
-        if (reason && reason.redirect) {
-          res.redirect(reason.redirect);
-        } else {
-          console.error('API ERROR:', pretty.render(reason));
-          res.status(reason.code || 500).json(reason);
-        }
+    res.header('Access-Control-Allow-Origin', '*');
+
+    if (action) {
+      action(req, params)
+        .then((result) => {
+          if (result instanceof Function) {
+            result(res);
+          } else {
+            res.json(result);
+          }
+        }, (reason) => {
+          if (reason && reason.redirect) {
+            res.redirect(reason.redirect);
+          } else {
+            console.error('API ERROR:', pretty.render(reason));
+            res.status(reason.code || 500).json(reason);
+          }
+        });
+    } else {
+      res.status(404).json({
+        code: 404,
+        message: 'Not found',
       });
-  } else {
-    res.status(404).json({
-      code: 404,
-      message: 'Not found',
-    });
-  }
-});
+    }
+  });
 
 if (config.appApiPort) {
   app.listen(config.appApiPort, (err) => {

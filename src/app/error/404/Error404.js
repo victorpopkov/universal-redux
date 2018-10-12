c973f205dd52f3ca8abbd26cd455f3420a57c18d
Victor Popkov <victor@popkov.me>
2018-10-12 11:38:29 +0300

    Add app error 404

---
+++ src/app/error/404/Error404.js
@@ -0,0 +1,26 @@
+import './Error404.scss';
+import { Button, Container } from 'reactstrap';
+import Helmet from 'react-helmet';
+import { IndexLinkContainer } from 'react-router-bootstrap';
+import React from 'react';
+import { hot } from 'react-hot-loader';
+
+const Error404 = () => (
+  <main role="main" styleName="error">
+    <Helmet title="Error 404 – Not Found" />
+    <Container className="text-center">
+      <h1>404</h1>
+      <h2>Page Not Found</h2>
+      <IndexLinkContainer to="/">
+        <Button
+          color="secondary"
+          outline
+        >
+          <span>&larr; Back to Homepage</span>
+        </Button>
+      </IndexLinkContainer>
+    </Container>
+  </main>
+);
+
+export default hot(module)(Error404);
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 import App from './app/App';
+import Error404 from './app/error/404/Error404';
 import home from './app/home/routes';
 
 export default [{
   component: App,
   routes: [
     ...home,
+    {
+      path: '*',
+      component: Error404,
+    },
   ],
 }];

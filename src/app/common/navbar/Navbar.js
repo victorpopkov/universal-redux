a90a02be64d1875266cdb3e5f97d8123b9557e46
Victor Popkov <victor@popkov.me>
2018-10-12 11:32:48 +0300

    Improve app common navbar

--- src/app/common/navbar/Navbar.js
+++ src/app/common/navbar/Navbar.js
@@ -1,16 +1,78 @@
-import { Navbar as BaseNavbar, Container, NavbarBrand } from 'reactstrap';
-import { IndexLinkContainer } from 'react-router-bootstrap';
-import React from 'react';
-import packageJson from '@Root/package.json';
+import {
+  Navbar as BaseNavbar,
+  Collapse,
+  Container,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarBrand,
+  NavbarToggler,
+} from 'reactstrap';
+import { IndexLinkContainer, LinkContainer } from 'react-router-bootstrap';
+import React, { Component } from 'react';
+import config from '@Config';
 
-const Navbar = () => (
-  <BaseNavbar color="faded" expand="md" light>
-    <Container className="text-center">
-      <IndexLinkContainer to="/">
-        <NavbarBrand className="ml-auto mr-auto">{packageJson.name}</NavbarBrand>
-      </IndexLinkContainer>
-    </Container>
-  </BaseNavbar>
-);
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+
+    this.state = {
+      collapsed: false,
+    };
+  }
+
+  toggleNavbar() {
+    this.setState(state => ({
+      collapsed: !state.collapsed,
+    }));
+  }
+
+  render() {
+    const { collapsed } = this.state;
+
+    return (
+      <BaseNavbar expand="md" light>
+        <Container>
+          <IndexLinkContainer to="/">
+            <NavbarBrand>{config.package.name}</NavbarBrand>
+          </IndexLinkContainer>
+          <NavbarToggler onClick={this.toggleNavbar} />
+          <Collapse isOpen={collapsed} timeout={0} navbar>
+            <div className="d-block d-md-inline-block ml-auto text-center">
+              <hr className="d-block d-sm-none" />
+              <Nav navbar>
+                <NavItem>
+                  <NavLink href={config.package.repository}>GitHub</NavLink>
+                </NavItem>
+              </Nav>
+            </div>
+            <div className="d-md-none d-sm-block ml-auto text-center">
+              <hr />
+              <h6>Getting Started</h6>
+              <Nav navbar>
+                <NavItem>
+                  <IndexLinkContainer to="/">
+                    <NavLink>Overview</NavLink>
+                  </IndexLinkContainer>
+                </NavItem>
+              </Nav>
+              <hr />
+              <h6>Examples</h6>
+              <Nav navbar>
+                <NavItem>
+                  <LinkContainer to="/404">
+                    <NavLink>404 Not Found</NavLink>
+                  </LinkContainer>
+                </NavItem>
+              </Nav>
+            </div>
+          </Collapse>
+        </Container>
+      </BaseNavbar>
+    );
+  }
+}
 
 export default Navbar;

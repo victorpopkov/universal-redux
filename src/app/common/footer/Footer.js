a0fe7e4e12c779f7de1ba262bf19120280cf3458
Victor Popkov <victor@popkov.me>
2018-10-12 11:32:39 +0300

    Improve app common footer

--- src/app/common/footer/Footer.js
+++ src/app/common/footer/Footer.js
@@ -1,32 +1,41 @@
 import './Footer.scss';
+import {
+  Col,
+  Container,
+  Row,
+} from 'reactstrap';
 import React from 'react';
-import packageJson from '../../../../package.json';
+import config from '@Config';
 
 const Footer = () => (
   <footer className="footer" styleName="footer">
-    <div className="container">
-      <ul>
-        <li><a href={`https://github.com/victorpopkov/${packageJson.name}`}>View on GitHub</a></li>
-      </ul>
-      <div styleName="buttons">
-        <iframe
-          frameBorder="0"
-          height="20px"
-          scrolling="0"
-          src={`https://ghbtns.com/github-btn.html?user=victorpopkov&repo=${packageJson.name}&type=star&count=true`}
-          title="GitHub Stars"
-          width="80px"
-        />
-        <iframe
-          frameBorder="0"
-          height="20px"
-          scrolling="0"
-          src={`https://ghbtns.com/github-btn.html?user=victorpopkov&repo=${packageJson.name}&type=watch&count=true&v=2`}
-          title="GitHub Watchers"
-          width="90px"
-        />
-      </div>
-    </div>
+    <Container>
+      <Row>
+        <Col md={12} styleName="content">
+          <ul>
+            <li><a href={config.package.repository}>View on GitHub</a></li>
+          </ul>
+          <div styleName="buttons">
+            <iframe
+              frameBorder="0"
+              height="20px"
+              scrolling="0"
+              src={`https://ghbtns.com/github-btn.html?user=victorpopkov&repo=${config.package.name}&type=star&count=true`}
+              title="GitHub Stars"
+              width="80px"
+            />
+            <iframe
+              frameBorder="0"
+              height="20px"
+              scrolling="0"
+              src={`https://ghbtns.com/github-btn.html?user=victorpopkov&repo=${config.package.name}&type=watch&count=true&v=2`}
+              title="GitHub Watchers"
+              width="90px"
+            />
+          </div>
+        </Col>
+      </Row>
+    </Container>
   </footer>
 );
 

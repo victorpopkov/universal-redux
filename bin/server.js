fa45ec422eb4e88ac5283b4bb81c1f8f54a1779f
Victor Popkov <victor@popkov.me>
2021-03-18 09:33:50 +0200

    Fix require of configuration in bin/server.js

--- bin/server.js
+++ bin/server.js
@@ -4,8 +4,16 @@ require('@babel/register');
 
 const dotenv = require('dotenv');
 const startServer = require('universal-webpack/server');
-const configuration = require('../webpack/webpack.config.server.dev.babel');
 const settings = require('../webpack/universal-webpack-settings');
+
+let configuration;
+if (process.env.NODE_ENV === 'production') {
+  // eslint-disable-next-line
+  configuration = require('../webpack/webpack.config.server.prod.babel');
+} else {
+  // eslint-disable-next-line
+  configuration = require('../webpack/webpack.config.server.dev.babel');
+}
 
 // dotenv
 const result = dotenv.config();

4d07ae92d084955c4e30387cd1df200328a82ef0
Victor Popkov <victor@popkov.me>
2021-03-31 06:18:42 +0300

    Remove configurations .babelrc in favour of babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,75 @@
+const path = require('path');
+
+const CSS_MODULE_LOCAL_IDENT_NAME = '[local]___[hash:base64:5]';
+
+module.exports = (api) => {
+  api.cache(true);
+  api.assertVersion('^7.4.5');
+
+  const presets = [
+    [
+      '@babel/env',
+      {
+        targets: {
+          browsers: ['last 2 versions'],
+        },
+      },
+    ],
+    '@babel/react',
+  ];
+
+  const plugins = [
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: false }],
+    [
+      'webpack-alias',
+      { config: path.join(__dirname, '/webpack/webpack.config.common.js') },
+    ],
+    '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-proposal-function-bind',
+    '@babel/plugin-syntax-export-default-from',
+    '@babel/plugin-transform-react-display-name',
+    '@babel/plugin-transform-runtime',
+    'react-hot-loader/babel',
+    [
+      'inline-react-svg',
+      {
+        svgo: {
+          plugins: [
+            {
+              name: 'removeAttrs',
+              params: { attrs: '(data-name)' },
+            },
+            'cleanupIDs',
+          ],
+        },
+      },
+    ],
+    [
+      'prismjs',
+      {
+        languages: ['bash', 'markdown'],
+        theme: 'default',
+        css: true,
+      },
+    ],
+    [
+      'react-css-modules',
+      {
+        generateScopedName: CSS_MODULE_LOCAL_IDENT_NAME,
+        filetypes: {
+          '.scss': {
+            plugins: ['postcss-nested'],
+            syntax: 'postcss-scss',
+          },
+        },
+      },
+    ],
+  ];
+
+  return {
+    compact: false,
+    presets,
+    plugins,
+  };
+};

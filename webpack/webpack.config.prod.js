6f1584a4cf30d2c56190dd0c5afb2c408948cad1
Victor Popkov <victor@popkov.me>
2021-05-20 08:35:24 +0300

    Update requires order in webpack prod configs

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,5 +1,5 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const { merge } = require('webpack-merge');
 const path = require('path');

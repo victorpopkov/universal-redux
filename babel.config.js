3dc888410cbb641fb657e267960b7e827d75edb2
Victor Popkov <victor@popkov.me>
2021-08-19 04:46:19 +0300

    Remove react-css-modules from Babel configs

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,4 @@
-const genericNames = require('generic-names');
 const path = require('path');
-
-const CSS_MODULE_LOCAL_IDENT_NAME = '[local]___[hash:base64:5]';
 
 module.exports = (api) => {
   api.cache(true);
@@ -45,18 +42,6 @@ module.exports = (api) => {
         languages: ['bash', 'markdown'],
         theme: 'default',
         css: true,
-      },
-    ],
-    [
-      'react-css-modules',
-      {
-        generateScopedName: genericNames(CSS_MODULE_LOCAL_IDENT_NAME),
-        filetypes: {
-          '.scss': {
-            plugins: ['postcss-nested'],
-            syntax: 'postcss-scss',
-          },
-        },
       },
     ],
   ];

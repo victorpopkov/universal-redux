64f3f669d9862b45aca1336645c7469c35952486
Victor Popkov <victor@popkov.me>
2023-03-13 08:10:06 +0200

    Improve Prettier issues output for lint job in ci GA workflow
    - Add ci:prettier script in package.json
    - Add prettier-output.js
    - Change lint job respectively in ci GA workflow
    - Remove ci:prettier:* scripts in package.json

---
+++ .github/prettier-output.js
@@ -0,0 +1,27 @@
+const core = require('@actions/core');
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec);
+
+const getNrOfIssues = async () => {
+  let result = {};
+
+  try {
+    result = await exec(`prettier --check .`);
+  } catch (error) {
+    result.stderr = error.stderr;
+  }
+
+  if (result.stderr.length > 0) {
+    return result.stderr.trim().split(/\r\n|\r|\n/).length - 1;
+  }
+
+  return 0;
+};
+
+const run = async () => ({
+  total: (await getNrOfIssues()) || 0,
+});
+
+run().then((result) => {
+  core.setOutput('prettier-issues', result.total);
+});

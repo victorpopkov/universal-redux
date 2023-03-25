82a838973aab62cdee5dfeedaeb3ed89389a44ef
Victor Popkov <victor@popkov.me>
2023-03-25 22:37:50 +0200

    Add support for loading GitHub versions and revisions in App container

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Helmet from 'react-helmet';
 import { ImmutableLoadingBar as LoadingBar } from 'react-redux-loading-bar';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import { hot } from 'react-hot-loader'; // eslint-disable-line import/no-extraneous-dependencies
 import { renderRoutes } from 'react-router-config';
 import config from '@Config';
@@ -10,6 +11,61 @@ import { Footer, Navbar } from './common'; // eslint-disable-line sort-imports
 @hot(module)
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
+  componentDidMount() {
+    this.latestDevRevision = null;
+    this.latestDevVersion = null;
+    this.latestStableRevision = null;
+    this.latestStableVersion = null;
+
+    const api = axios.create({
+      baseURL: `https://api.github.com/repos/${config.package.githubRef}`,
+      headers: {
+        Accept: 'application/vnd.github.v3+json',
+      },
+    });
+
+    api.get(`/contents/package.json`).then((res) => {
+      if (res.data.encoding === null) {
+        return;
+      }
+
+      switch (res.data.encoding) {
+        case 'json':
+          this.latestDevVersion = res.data.version;
+          this.forceUpdate();
+          break;
+        case 'base64':
+          this.latestDevVersion = JSON.parse(atob(res.data.content)).version;
+          this.forceUpdate();
+          break;
+        default:
+      }
+    });
+
+    api.get(`/commits`).then((res) => {
+      this.latestDevRevision = res.data[0].sha;
+      this.forceUpdate();
+    });
+
+    api.get(`/tags`).then((res) => {
+      let tag = null;
+
+      if (res.data.length > 0) {
+        res.data.forEach((data) => {
+          if (tag === null && data.name.match(/^v/)) {
+            tag = data;
+          }
+        });
+      }
+
+      if (tag !== null) {
+        this.latestStableVersion = tag.name.replace(/^v/, '');
+        this.latestStableRevision = tag.commit.sha;
+        this.forceUpdate();
+      }
+    });
+  }
+
   render() {
     const {
       route: { routes },
@@ -23,7 +79,12 @@ class App extends Component {
         </Helmet>
         <LoadingBar className="loading" />
         <Navbar />
-        {renderRoutes(routes)}
+        {renderRoutes(routes, {
+          latestDevRevision: this.latestDevRevision,
+          latestDevVersion: this.latestDevVersion,
+          latestStableRevision: this.latestStableRevision,
+          latestStableVersion: this.latestStableVersion,
+        })}
         <Footer repository={config.package.repository} />
       </div>
     );

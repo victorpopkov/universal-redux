773823fdf602f16f9f2c3c751e1f93ab7002be5c
Victor Popkov <victor@popkov.me>
2024-01-19 20:56:43 +0200

    Add CORS headers and fix middlewares in webpack webpack-dev-server configs
    - Add Access-Control-Allow-Headers header
    - Add Access-Control-Allow-Methods header
    - Add explicit path for webpack-hot-middleware
    - Fix Access-Control-Allow-Origin header
    - Fix middlewares order

--- webpack/webpack-dev-server.js
+++ webpack/webpack-dev-server.js
@@ -1,26 +1,39 @@
 require('@babel/register');
 
+/* eslint-disable import/no-extraneous-dependencies */
 const Express = require('express');
-const middleware = require('webpack-dev-middleware'); // eslint-disable-line import/no-extraneous-dependencies
-const webpack = require('webpack'); // eslint-disable-line import/no-extraneous-dependencies
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
 const devClient = require('./dev.client.config');
 const config = require('../config');
+/* eslint-enable import/no-extraneous-dependencies */
 
 const app = new Express();
 const compiler = webpack(devClient);
 const port = config.appDevServerPort;
 
-app
-  .use(
-    middleware(compiler, {
-      headers: { 'Access-Control-Allow-Origin': '*' },
-    }),
-  )
-  .use(middleware(compiler))
-  .listen(port, (err) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.info('==> webpack-dev-server listening on port %s', port);
-    }
-  });
+const setCorsHeaders = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+  );
+  res.header(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With, Content-Type, Authorization',
+  );
+  next();
+};
+
+app.use(setCorsHeaders);
+app.use(webpackDevMiddleware(compiler));
+app.use(webpackHotMiddleware(compiler, { path: '/__webpack_hmr' }));
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.info('==> webpack-dev-server listening on port %s', port);
+  }
+});
